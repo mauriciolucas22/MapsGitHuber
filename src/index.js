87ba450a8a44e800eb111984228f35a5e3a09cd7
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-01-03 11:03:40 -0200

    index in src, Reactotron instaled

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+import "./config/ReactotronConfig";
+
+class App extends Component {
+  render() {
+    return(
+      <View>
+        <Text>Jesus</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
