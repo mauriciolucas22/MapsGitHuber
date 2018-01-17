ad14ee058be58eec59faa284a3cfc8c8c890e158
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-01-17 10:21:04 -0200

    maps installed for android

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import { View, Text } from 'react-native';
 
 import "./config/ReactotronConfig";
 
+
 class App extends Component {
   render() {
     return(
       <View>
-        <Text>Jesus</Text>
+        <Text>Jesus Cristo, TESTE</Text>
       </View>
     );
   }
