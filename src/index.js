850537f44bd1c49190e2b5454098926a6d9ba9dd
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-01-19 10:45:42 -0200

    onLongPress with openModal

--- src/index.js
+++ src/index.js
@@ -1,14 +1,81 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Button, Modal } from 'react-native';
+import MapView from 'react-native-maps';
 
 import "./config/ReactotronConfig";
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'grey'
+  },
+  innerContainer: {
+    alignItems: 'center',
+  },
+});
+
 
 class App extends Component {
+
+  state = {
+    modalVisible: false,
+  }
+
+
+  openModal() {
+    this.setState({ modalVisible: true });
+  }
+
+  closeModal() {
+    this.setState({ modalVisible: false });
+  }
+
+
   render() {
+
+    const { region } = this.props;
+    console.log(region);
+
     return(
-      <View>
-        <Text>Jesus Cristo, TESTE</Text>
+      <View style={styles.container}>
+
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: -27.2177659,
+            longitude: -49.6451598,
+            latitudeDelta: 0.0042,
+            longitudeDelta: 0.0031,
+          }}
+          onLongPress={() => this.openModal()}
+        >
+        </MapView>
+
+
+        <Modal
+              visible={this.state.modalVisible}
+              animationType={'slide'}
+              onRequestClose={() => this.closeModal()}
+          >
+            <View style={styles.modalContainer}>
+              <View style={styles.innerContainer}>
+                <Text>This is content inside of modal component</Text>
+                <Button
+                    onPress={() => this.closeModal()}
+                    title="Close modal"
+                >
+                </Button>
+              </View>
+            </View>
+          </Modal>
       </View>
     );
   }
