a40f130e1022546a19029c42ea715386cfa33aa6
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-01-20 11:50:47 -0200

    reducers created

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+import newUser from './newUser';
+
+export default combineReducers({
+  newUser,
+});
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
-//const sagaMonitor = __DEV__ ? console.tron.createSagaMonitor() : null;
-
-//middleware
-//const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+// Reducers
+import reducers from './reducers';
 
 // Array de middleware
-//const middleware = [sagaMiddleware];
 const middleware = [];
 
-//
-//const createAppropriateStore = __DEV__ ? console.tron.createStore : createStore;
 
-// sem reducers, applyMiddleware para api
-//const store = createAppropriateStore(() => {}, applyMiddleware(...middleware));
-const store = createStore(() => {}, applyMiddleware(...middleware));
+const store = createStore(reducers, applyMiddleware(...middleware));
 
 export default store;
